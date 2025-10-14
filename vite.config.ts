61ae4c90ce67fd024921facfa5789977a9baa3c9
vaibhavpokhriyal13 <vaibhavpokhriyal13@gmail.com>
2025-10-14 08:06:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "h6wyr7-8080.csb.app",
     port: 8080,
   },
   plugins: [
