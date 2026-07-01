a7a55c94e5136afe3675cfb62d3f4fa4478fc4ef
vaibhavpokhriyal13 <vaibhavpokhriyal13@gmail.com>
2026-07-01 10:16:30 +0530

    made changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({
@@ -16,7 +16,7 @@ export default defineConfig(({
       }
     }
   },
-  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
